5ae59b011d30a61016fabac9ab8d211291725e81
kmac007 <robbchan@foxmail.com>
2017-08-07 16:23:24 +0800

    增加移动端适配方案，并完成头部和导航条的布局

--- src/js/components/mobile/mobile_header.js
+++ src/js/components/mobile/mobile_header.js
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import 'css/mobile/mobile_header.scss'
 class MobileHeader extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      isSearching: false
+    }
+  }
   render(){
     return (
-      <div>我是移动头</div>
+      <header className="header">
+        <h1 className='mobile-logo'>新闻头条</h1>
+        <div className="search-box">
+          <span>
+            <i className="iconfont icon-search3"></i>
+          </span>
+        </div>
+      </header>
     )
   }
 }
--- src/js/components/mobile/mobile_nav.js
+++ src/js/components/mobile/mobile_nav.js
@@ -1,11 +1,55 @@
-import React from 'react'
-
-class MobileNav extends React.Component{
-  render(){
+import React from 'react';
+import 'css/mobile/mobile_nav.scss';
+class MobileNav extends React.Component {
+  constructor() {
+    super();
+    this.showMore = this.showMore.bind(this);
+    this.state = {
+      isShow: false
+    };
+  }
+  showMore() {
+    this.state.isShow
+      ? this.setState({
+          isShow: false
+        })
+      : this.setState({
+          isShow: true
+        });
+  }
+  render() {
     return (
-      <div>我是移动导航</div>
-    )
+      <div className="mobile-nav">
+        <ul className="nav-list-top">
+          <li>热点</li>
+          <li>体育</li>
+          <li>军事</li>
+          <li>国内</li>
+          <li onClick={this.showMore}>
+            更多{' '}
+            <i
+              className={
+                this.state.isShow
+                  ? 'iconfont icon-open'
+                  : 'iconfont icon-open flip'
+              }
+            />
+          </li>
+        </ul>
+        <ul
+          className={
+            this.state.isShow ? 'nav-list-bottom' : 'nav-list-bottom-hidden'
+          }
+        >
+          <li>国际</li>
+          <li>娱乐</li>
+          <li>社会</li>
+          <li>游戏</li>
+          <li>科技</li>
+        </ul>
+      </div>
+    );
   }
 }
 
-export default MobileNav
+export default MobileNav;
